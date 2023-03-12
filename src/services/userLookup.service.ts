5c21d9b161c5fec7b16f8d00bcfe631756261c83
princechibez <chibezprince@gmail.com>
2023-03-12 13:37:54 -0700

    All requirements and functionalities integrated

--- src/services/userLookup.service.ts
+++ src/services/userLookup.service.ts
@@ -3,7 +3,7 @@
 // update a post
 // delete a post
 
-import { Postit, User } from "../models";
+import { Comment, Postit, User } from "../models";
 import { IPostit } from "../interfaces/post.interface";
 import { IErrorObj } from "../interfaces/error.interface";
 import { IUserQueryProps } from "../interfaces/user.interface";
@@ -32,6 +32,58 @@ class USER_LOOKUP_SERVICE {
                     .sort({ createdAt: -1 })
             }
             return userPostits
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    // get all postits of a user
+    async getOneUserPostit(userId: string, postitId: string) {
+        try {
+            let userPostit = await Postit.findOne(
+                { creator: userId, _id: postitId, deleted: false }
+            )
+                .select("-deleted")
+                .populate({ path: "creator", select: "username profilePicture profileImageTag" })
+                .lean()
+
+            return userPostit
+        } catch (err) {
+            throw err;
+        }
+    }
+    
+    // get all comments of a user's postit
+    async getUserPostitComments(userId: string, postitId: string) {
+        try {
+            let comments = await Comment.find(
+                { commentator: userId, postId: postitId, deleted: false }
+            )
+                .select("-deleted")
+                .populate({ path: "commentator",
+                 select: "username profilePicture profileImageTag" })
+                .lean()
+
+            return comments
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    // get a single comment of a user's postit
+    async getUserPostitComment(userId: string, postitId: string, commentId: string) {
+        try {
+            let comment = await Comment.findOne(
+                { commentator: userId, postId: postitId, _id: commentId, deleted: false }
+            )
+                .select("-deleted")
+                .populate({
+                    path: "commentator",
+                    select: "username profilePicture profileImageTag"
+                })
+                .lean()
+
+            return comment
         } catch (err) {
             throw err;
         }
